bae2d49e75c2ef19f1458bdcd9cbbee6feb85add
192114 <shadowSun192114@gmail.com>
2022-05-27 18:38:52 +0800

    文章目录

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import Github from '@/icons/github.svg'
 
 const Footer = () => {
   return (
-    <footer className="py-12 flex-col items-center space-y-4">
+    <footer className="py-6 flex-col items-center space-y-3">
       <div className="space-x-4 flex items-center justify-center">
         <a
           href="mailto:shadowSun192114@gmail.com"
           className="text-sm text-gray-500 transition hover:text-gray-600"
         >
-          <Mail className="fill-current w-8 h-8 dark:text-gray-200 dark:hover:text-blue-400" />
+          <Mail className="fill-current w-7 h-7 dark:text-gray-200 dark:hover:text-blue-400" />
         </a>
 
         <a
@@ -18,18 +18,19 @@ const Footer = () => {
           rel="noreferrer"
           className="text-sm text-gray-500 transition hover:text-gray-600"
         >
-          <Github className="fill-current w-8 h-8 dark:text-gray-200 dark:hover:text-blue-400" />
+          <Github className="fill-current w-7 h-7 dark:text-gray-200 dark:hover:text-blue-400" />
         </a>
       </div>
 
-      <div className="flex items-center justify-center space-x-2 text-base text-gray-500 dark:text-gray-400">
+      <div className="flex items-center justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
         <span>PowerBy</span>
-        <a href="https://nextjs.org" target="_blank" rel="noreferrer">Next.js</a>
-        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">Tailwindcss</a>
+        <a href="https://nextjs.org" target="_blank" rel="noreferrer">Nextjs</a>
+        <a href="https://tailwindcss.com" target="_blank" rel="noreferrer">Tailwind</a>
+        <a href="https://github.com/timlrx/tailwind-nextjs-starter-blog" target="_blank" rel="noreferrer">Next.js Starter Blog</a>
       </div>
 
       <div className="flex items-center justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
-        
+        ©2022 Shadow Sun 
       </div>
     </footer>
   )
--- src/components/MdxLayout.tsx
+++ src/components/MdxLayout.tsx
@@ -1,5 +1,5 @@
 import { ComponentMap, getMDXComponent } from 'mdx-bundler/client'
-import { FC, useMemo } from 'react'
+import { FC, useMemo, useEffect, useRef, useState } from 'react'
 import Container from '@/components/Container'
 import formatDate from '@/lib/formatDate'
 import PageTitle from '@/components/PageTitle'
@@ -8,6 +8,7 @@ import Tag from '@/components/Tag'
 import { Toc } from 'types/Toc'
 import { PostFrontMatter } from 'types/PostFrontMatter'
 import cn from 'classnames'
+import { throttle } from 'lodash'
 
 interface WrapperProps {
   frontMatter: PostFrontMatter
@@ -18,6 +19,38 @@ interface WrapperProps {
 
 const Wrapper: FC<WrapperProps> = ({ frontMatter, next, prev, toc, children }) => {
   const { date, title, tags } = frontMatter
+  const [currentAnchor, setcurrentAnchor] = useState<string | null>(null)
+
+  const articleDomRef = useRef<HTMLDivElement | null>(null)
+
+  // 页面渲染后监听 滚动事件
+  useEffect(() => {
+    const scrollHandle = () => {
+      const docScrollTop = document.documentElement.scrollTop || document.body.scrollTop
+
+      let lowerZero: {value: string, top: number } | null = null
+      toc.forEach((tocItem) => {
+        const current = articleDomRef.current?.querySelector(`a[href="${tocItem.url}"]`)
+
+        if (current && current.getBoundingClientRect().top <= 0 && (!lowerZero || current.getBoundingClientRect().top > lowerZero.top)) {
+          lowerZero = { value: tocItem.url, top: current.getBoundingClientRect().top }
+        }
+      })
+
+      if (lowerZero !== null) {
+        setcurrentAnchor(lowerZero.value)
+      }
+      
+    }
+
+    document.addEventListener('scroll', scrollHandle, true)
+
+
+    return () => {
+      document.removeEventListener('scroll', scrollHandle, true)
+    }
+  }, [toc])
+  
 
   return (
     <Container>
@@ -35,84 +68,88 @@ const Wrapper: FC<WrapperProps> = ({ frontMatter, next, prev, toc, children }) =
 
           <div className="grid-cols-4 grid grid-rows-auto-1fr xl:gap-x-6 pb-5">
             {/* pc端显示在页面左侧 手机端显示在底部 */}
-            <footer className="col-span-4 row-start-2 divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-span-1 xl:divide-y xl:row-start-1">
-              {toc && (
-                <div className="py-4 xl:py-8 hidden xl:block">
-                  <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                    目录
-                  </h2>
-                  <div className="mt-3 flex flex-wrap text-sm text-gray-600 dark:text-gray-500">
-                    <dl className="space-y-2">
-                      {toc.map((item) => (
-                        <dd
-                          key={item.url}
-                          className={cn('truncate', {
-                            'pl-0': item.depth === 1,
-                            'pl-2': item.depth === 2,
-                            'pl-4': item.depth === 3,
-                            'pl-6': item.depth === 4,
-                          })}
-                        >
-                          <a href={item.url} className="hover:text-primary-500">{item.value}</a>
-                        </dd>
-                      ))}
-                    </dl>
-                  </div>
-                </div>
-              )}
-
-              <div>
-                {tags && (
-                  <div className="py-4 xl:py-8">
+            {/* 套一层div的目的是 sticky 定位的元素 与 父元素同高 导致定位失效  https://www.zhangxinxu.com/wordpress/2020/03/position-sticky-rules/ */}
+            <div className="h-auto col-span-4 row-start-2 xl:row-start-1 xl:col-span-1">
+              <footer className="sticky top-5  divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700  xl:divide-y ">
+                {/* 使用sticky 定位实现吸顶 */}
+                {toc && (
+                  <div className="py-4 xl:py-8 hidden xl:block">
                     <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                      标签
+                      目录
                     </h2>
-                    <div className="mt-3 flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
+                    <div className="mt-3 flex flex-wrap text-sm text-gray-600 dark:text-gray-500">
+                      <dl className="space-y-2">
+                        {toc.map((item) => (
+                          <dd
+                            key={item.url}
+                            className={cn('truncate', {
+                              'pl-0': item.depth === 1,
+                              'pl-4': item.depth === 2,
+                              'pl-8': item.depth === 3,
+                              'pl-12': item.depth === 4,
+                            })}
+                          >
+                            <a href={item.url} title={item.value} className="hover:text-primary-500">{item.value}</a>
+                          </dd>
+                        ))}
+                      </dl>
                     </div>
                   </div>
                 )}
 
-                {(next || prev) && (
-                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
-                    {prev && (
-                      <div>
-                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                          上一篇 文章
-                        </h2>
-                        <div className="mt-3 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                          <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
-                        </div>
+                <div>
+                  {tags && (
+                    <div className="py-4 xl:py-8">
+                      <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                        标签
+                      </h2>
+                      <div className="mt-3 flex flex-wrap">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
                       </div>
-                    )}
-                    {next && (
-                      <div>
-                        <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
-                          下一篇 文章
-                        </h2>
-                        <div className="mt-3 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                          <Link href={`/blog/${next.slug}`}>{next.title}</Link>
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                )}
-              </div>
+                    </div>
+                  )}
 
-              <div className="pt-4 xl:pt-8">
-                <Link href="/">
-                  <a className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-                    &larr; 返回博客列表
-                  </a>
-                </Link>
-              </div>
-            </footer>
+                  {(next || prev) && (
+                    <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                      {prev && (
+                        <div>
+                          <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                            上一篇 文章
+                          </h2>
+                          <div className="mt-3 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                            <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
+                          </div>
+                        </div>
+                      )}
+                      {next && (
+                        <div>
+                          <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                            下一篇 文章
+                          </h2>
+                          <div className="mt-3 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                            <Link href={`/blog/${next.slug}`}>{next.title}</Link>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
+                </div>
+
+                <div className="pt-4 xl:pt-8">
+                  <Link href="/">
+                    <a className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                      &larr; 返回博客列表
+                    </a>
+                  </Link>
+                </div>
+              </footer>
+            </div>
 
             {/* 中间文章显示区域 */}
             <div className="col-span-4 xl:col-span-3 xl:pb-0 divide-y divide-gray-200 dark:divide-gray-700">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark" ref={articleDomRef}>{children}</div>
             </div>
           </div>
         </div>
