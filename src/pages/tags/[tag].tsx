428dbe4828779a632402819622ee93db621cf293
192114 <shadowSun192114@gmail.com>
2022-06-01 14:52:48 +0800

    test

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -1,11 +1,10 @@
-import PostListLayout from "@/layouts/PostListLayout"
-import getAllTags from "@/lib/getAllTags"
-import kebabCase from "@/lib/kebabCase"
-import { getAllFilesFrontMatter } from "@/lib/mdx"
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next"
-import { ParsedUrlQuery } from "querystring"
-import { PostFrontMatter } from "types/PostFrontMatter"
-
+import PostListLayout from '@/layouts/PostListLayout'
+import getAllTags from '@/lib/getAllTags'
+import kebabCase from '@/lib/kebabCase'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+import { PostFrontMatter } from 'types/PostFrontMatter'
 
 export const getStaticPaths: GetStaticPaths = () => {
   const allTagsObj = getAllTags()
@@ -13,7 +12,7 @@ export const getStaticPaths: GetStaticPaths = () => {
   const paths = Object.keys(allTagsObj).map((tag) => ({
     params: {
       tag,
-    }
+    },
   }))
 
   return {
@@ -26,31 +25,32 @@ interface StaticParamProps extends ParsedUrlQuery {
   tag: string
 }
 
-export const getStaticProps: GetStaticProps<{
-  displayPosts: PostFrontMatter[]
-  tag: string
-}, StaticParamProps> = async (context) => {
+export const getStaticProps: GetStaticProps<
+  {
+    displayPosts: PostFrontMatter[]
+    tag: string
+  },
+  StaticParamProps
+> = async (context) => {
   const allPost = await getAllFilesFrontMatter()
-  const { tag : curTag } = context.params as StaticParamProps
+  const { tag: curTag } = context.params as StaticParamProps
 
-  const postFilter = allPost.filter((post) => post.draft !== true && post.tags.map(tag => kebabCase(tag)).includes(curTag))
+  const postFilter = allPost.filter(
+    (post) => post.draft !== true && post.tags.map((tag) => kebabCase(tag)).includes(curTag)
+  )
 
   return {
     props: {
       displayPosts: postFilter,
       tag: curTag,
-    }
+    },
   }
 }
-
 
 const Tags = ({ displayPosts, tag }: InferGetStaticPropsType<GetStaticProps>) => {
   return (
     <>
-      <PostListLayout 
-        pageTitle={tag}
-        posts={displayPosts}
-      />
+      <PostListLayout pageTitle={tag} posts={displayPosts} />
     </>
   )
 }
