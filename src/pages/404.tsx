98b97ce147b4f355a479660235f14834c429b2a2
192114 <shadowSun192114@gmail.com>
2022-06-10 18:12:49 +0800

    原生threejs实现

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,50 +1,89 @@
 import Link from '@/components/Link'
-import { Vector3 } from 'three'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Text3D, Center, Float, OrbitControls } from '@react-three/drei'
-import { useRef, Suspense } from 'react'
-
-const orbitControlsProps = {
-  // 上下旋转角度
-  minPolarAngle: Math.PI * (60 / 360),
-  maxPolarAngle: Math.PI * (300 / 360),
-  // 左右旋转角度
-  minAzimuthAngle: - Math.PI * (60 / 360),
-  maxAzimuthAngle: Math.PI * (60 / 360),
-
-  enablePan: false, // 禁止鼠标右键拖拽
-  enableZoom: false, // 禁止缩放大小
-}
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { useEffect, useRef } from 'react'
 
 const NotFound = () => {
-  return (
-    <div className="text-gray-900 dark:text-gray-100 h-96">
-      <Canvas
-        shadows
-        camera={{
-          position: new Vector3(0, 0, 40),
-          fov: 15,
-          // lookAt: () => new Vector3(0, 0, 0),
-        }}
-      >
-        <Suspense fallback={null}>
-          <Center>
-            <Float speed={3} floatIntensity={5}>
-              <Text3D font="/static/fonts/helvetiker_regular.json" size={2} height={1} bevelEnabled bevelSize={0.05}>
-                404
-                <meshNormalMaterial />
-              </Text3D>
-            </Float>
+  const containerRef = useRef<HTMLDivElement | null>(null)
 
-            <ambientLight intensity={0.8} />
-            <pointLight intensity={1} position={[0, 6, 0]} />
+  useEffect(() => {
+    const target = new THREE.Vector3(0, 0, 0)
+    const container = containerRef.current as HTMLDivElement
+    const containerWidth = container.offsetWidth
+    const containerHeight = container.offsetHeight
 
-            <OrbitControls {...orbitControlsProps} />
-          </Center>
-        </Suspense>
-      </Canvas>
-    </div>
-  )
+    // 创建场景
+    const scene = new THREE.Scene()
+    // 绘制辅助线
+    const axisHelper = new THREE.AxesHelper(250)
+    scene.add(axisHelper)
+
+    // 添加光源
+    const ambientLight = new THREE.AmbientLight(0xffffff) // 环境光会均匀的照亮场景中的所有物体。
+    scene.add(ambientLight)
+
+    // 渲染器
+    const renderer = new THREE.WebGLRenderer({
+      alpha: true, // 背景透明
+      antialias: true, // 抗锯齿
+    })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    // 设置尺寸
+    renderer.setSize(containerWidth, containerHeight)
+    container.appendChild(renderer.domElement)
+
+    // 创建模型
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshNormalMaterial()
+
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+    // 创建透视相机 （视角为15度）
+    const camera = new THREE.PerspectiveCamera(15, containerWidth / containerHeight, 1, 10000)
+    camera.position.set(0, 0, 10)
+    camera.lookAt(target)
+    renderer.render(scene, camera)
+
+    // 鼠标操作
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    controls.enableDamping = true // 动画循环是否有阻尼
+    // controls.autoRotate = true
+    // controls.dampingFactor = 0.05
+    // controls.screenSpacePanning = false
+    // controls.minDistance = 100
+    // controls.maxDistance = 500
+    // controls.maxPolarAngle = Math.PI / 2
+    // controls.target = target
+    // controls.update()
+
+    const render = () => {
+      // controls.update()
+      renderer.render(scene, camera)
+    }
+
+    const resizeWindow = () => {
+      const containerWidth = container.offsetWidth
+      const containerHeight = container.offsetHeight
+      renderer.setSize(containerWidth, containerHeight)
+      render()
+    }
+
+    // 必须注册该方法 否则无效
+    controls.addEventListener('change', render)
+
+    window.addEventListener('resize', resizeWindow, false)
+
+    return () => {
+      container.removeChild(renderer.domElement)
+      renderer.dispose()
+      controls.dispose()
+      window.removeEventListener('resize', resizeWindow, false)
+    }
+  }, [])
+
+  return <div ref={containerRef} className="text-gray-900 dark:text-gray-100 h-96"></div>
 }
 
 export default NotFound
